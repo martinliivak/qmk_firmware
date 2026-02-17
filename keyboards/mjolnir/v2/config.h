01ae9f3e0c085949933f0e508b6adfa5df7cbabf
MartinL <martin.liivak@gmail.com>
2026-02-17 20:52:41 +0200

    Created a working configuration for using PMW3360 on v2.

---
+++ keyboards/mjolnir/v2/config.h
@@ -0,0 +1,14 @@
+// Copyright 2026 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode.
+#define SERIAL_USART_TX_PIN GP0    // USART TX pin
+#define SERIAL_USART_RX_PIN GP1    // USART RX pin
+
+// PMW3360 config
+//#define PMW33XX_LIFTOFF_DISTANCE 0x00
+#define PMW33XX_CS_PIN GP21
+#define SPI_MISO_PIN GP23
+#define SPI_MOSI_PIN GP20
+#define SPI_SCK_PIN GP22
